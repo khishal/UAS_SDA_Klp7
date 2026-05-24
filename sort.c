871e81aaae3392559a317c5b902948685383fd9a
raisa fahira <raisafahira127@gmail.com>
2026-05-24 18:55:30 +0700

    Update sort.c

--- sort.c
+++ sort.c
@@ -29,3 +29,44 @@ void quicksort(Pengeluaran arr[], int low, int high) {
         quicksort(arr, pi + 1, high);
     }
 }
+void sort_dan_tampilkan(Konteks *ctx, int mode) {
+    if (ctx->jumlah_data == 0) {
+        printf("  (belum ada data)\n");
+        return;
+    }
+
+    Pengeluaran tmp[MAX_DATA];
+    int n = 0;
+
+    for (int i = 0; i < ctx->jumlah_data; i++) {
+        Pengeluaran *p = &ctx->array[i];
+        int masuk = 0;
+
+        switch (mode) {
+            case 0:
+                masuk = 1;
+                break;
+
+            case 1:
+                masuk = (p->kategori == KAT_SANDANG);
+                break;
+
+            case 2:
+                masuk = (p->kategori == KAT_PANGAN);
+                break;
+
+            default:
+                masuk = 1;
+        }
+
+        if (masuk) {
+            tmp[n++] = *p;
+        }
+    }
+
+    if (n == 0) {
+        printf("  (tidak ada data untuk kategori ini)\n");
+        return;
+    }
+
+    quicksort(tmp, 0, n - 1);
